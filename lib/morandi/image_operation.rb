cd870738120309af18628d648554a51a6c358efd
Geoff Youngs <git@intersect-uk.co.uk>
2021-02-26 15:45:23 +0000

    Refactor to fix rubocop complaints

--- lib/morandi/image_operation.rb
+++ lib/morandi/image_operation.rb
@@ -67,18 +67,81 @@ module Morandi
     def call(_image, pixbuf)
       return pixbuf unless %w[square retro].include? @style
 
-      surface = Cairo::ImageSurface.new(:rgb24, pixbuf.width, pixbuf.height)
-      cr = Cairo::Context.new(surface)
-
-      img_width = pixbuf.width
-      img_height = pixbuf.height
-
-      cr.save do
+      create_pixbuf_from_image_surface(:rgb24, pixbuf.width, pixbuf.height) do |cr|
         if @crop && ((@crop[0]).negative? || (@crop[1]).negative?)
           img_width = size[0]
           img_height = size[1]
-          cr.translate(- @crop[0], - @crop[1])
+        else
+          img_width = pixbuf.width
+          img_height = pixbuf.height
         end
+
+        @border_scale = [img_width, img_height].max.to_f / print_size.max.to_i
+
+        draw_background(cr, img_height, img_width, pixbuf)
+
+        x = border_width
+        y = border_width
+
+        # This biggest impact will be on the smallest side, so to avoid white
+        # edges between photo and border scale by the longest changed side.
+        longest_side = [pixbuf.width, pixbuf.height].max.to_f
+
+        # Should be less than 1
+        pb_scale = (longest_side - (border_width * 2)) / longest_side
+
+        if @crop && ((@crop[0]).negative? || (@crop[1]).negative?)
+          x -= @crop[0]
+          y -= @crop[1]
+        end
+
+        draw_pixbuf(pixbuf, cr, img_height, img_width, pb_scale, x, y)
+      end
+    end
+
+    private
+
+    def create_pixbuf_from_image_surface(type, width, height)
+      surface = Cairo::ImageSurface.new(type, width, height)
+      cr = Cairo::Context.new(surface)
+
+      yield(cr)
+
+      final_pb = surface.to_gdk_pixbuf
+      cr.destroy
+      surface.destroy
+      final_pb
+    end
+
+    # Width is proportional to output size
+    def border_width
+      @border_size * @border_scale
+    end
+
+    def draw_pixbuf(pixbuf, cr, img_height, img_width, pb_scale, x, y)
+      case style
+      when 'retro'
+        Morandi::CairoExt.rounded_rectangle(cr, x, y,
+                                            img_width + x - (border_width * 2),
+                                            img_height + y - (border_width * 2), border_width)
+      when 'square'
+        cr.rectangle(x, y, img_width - (border_width * 2), img_height - (border_width * 2))
+      end
+      cr.clip
+
+      if @shrink
+        cr.translate(border_width, border_width)
+        cr.scale(pb_scale, pb_scale)
+      end
+      cr.set_source_pixbuf(pixbuf)
+      cr.rectangle(0, 0, pixbuf.width, pixbuf.height)
+
+      cr.paint(1.0)
+    end
+
+    def draw_background(cr, img_height, img_width, pixbuf)
+      cr.save do
+        cr.translate(-@crop[0], -@crop[1]) if @crop && ((@crop[0]).negative? || (@crop[1]).negative?)
 
         cr.save do
           cr.set_operator :source
@@ -89,13 +152,9 @@ module Morandi
           case colour
           when 'dominant'
             pixbuf.scale_max(400).save(fn = "/tmp/hist-#{$PROCESS_ID}.#{Time.now.to_i}", 'jpeg')
-            hgram = Colorscore::Histogram.new(fn)
-            begin
-              File.unlink(fn)
-            rescue StandardError
-              nil
-            end
-            col = hgram.scores.first[1]
+            histogram = Colorscore::Histogram.new(fn)
+            FileUtils.rm_f(fn)
+            col = histogram.scores.first[1]
             cr.set_source_rgb col.red / 256.0, col.green / 256.0, col.blue / 256.0
           when 'retro'
             cr.set_source_rgb 1, 1, 0.8
@@ -107,47 +166,6 @@ module Morandi
           cr.fill
         end
       end
-
-      border_scale = [img_width, img_height].max.to_f / print_size.max.to_i
-      size = @border_size
-      size *= border_scale
-      x = size
-      y = size
-
-      # This biggest impact will be on the smallest side, so to avoid white
-      # edges between photo and border scale by the longest changed side.
-      longest_side = [pixbuf.width, pixbuf.height].max.to_f
-
-      # Should be less than 1
-      pb_scale = (longest_side - (size * 2)) / longest_side
-
-      if @crop && ((@crop[0]).negative? || (@crop[1]).negative?)
-        x -= @crop[0]
-        y -= @crop[1]
-      end
-
-      case style
-      when 'retro'
-        Morandi::CairoExt.rounded_rectangle(cr, x, y,
-                                            img_width + x - (size * 2),
-                                            img_height + y - (size * 2), size)
-      when 'square'
-        cr.rectangle(x, y, img_width - (size * 2), img_height - (size * 2))
-      end
-      cr.clip
-
-      if @shrink
-        cr.translate(size, size)
-        cr.scale(pb_scale, pb_scale)
-      end
-      cr.set_source_pixbuf(pixbuf)
-      cr.rectangle(0, 0, pixbuf.width, pixbuf.height)
-
-      cr.paint(1.0)
-      final_pb = surface.to_gdk_pixbuf
-      cr.destroy
-      surface.destroy
-      final_pb
     end
   end
 
