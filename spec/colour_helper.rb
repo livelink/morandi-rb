a6231ed99be7093d9cbda2c9e7fcc8d2e7c6d7fc
Geoff Youngs <git@intersect-uk.co.uk>
2021-02-26 14:59:10 +0000

    Refactor tests slightly & produce visual test report for verification

---
+++ spec/colour_helper.rb
@@ -0,0 +1,29 @@
+module ColourHelper
+  def generate_test_image(at_file_path, width = 600, height = 300)
+    system(
+      'convert',
+      '-size',
+      "#{width}x#{height}",
+      '-seed',
+      '5432',
+      'plasma:red-blue',
+      'pattern:checkerboard', '-gravity','center','-geometry', "+#{width*3/4},+0",'-composite',
+      at_file_path
+    )
+  end
+
+  def crude_average_colour(pixbuf)
+    get_pixels = lambda do |pb|
+      pb.pixels.each_slice(pb.rowstride).map do |row|
+        row.each_slice(3).to_a[0...pb.width]
+      end.to_a[0...pb.height].flatten(1)
+    end
+    avg_color = lambda do |pixels|
+      list = pixels.inject([0, 0, 0]) do |(br, bg, bb), (r, g, b)|
+        [br + r, bg + g, bb + b]
+      end
+      list.map { |a| (a / pixels.size.to_f).to_i }
+    end
+    avg_color.call(get_pixels.call(pixbuf))
+  end
+end
